6bee6596fada7b39ceb1dd1e75b2d2f6cf4c43a0
cody <git@codylambert.com>
2012-12-29 14:42:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodyTest::Application.config.secret_token = '06c2f0a5085c38796184728b0c77d307aac6128d662bec5e9b4fee45ea42734f5e8c338646f2df094d264e29380602f9dcf65cb43e1ad1a257f20bfd78f2509b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CodyTest::Application.config.session_store :cookie_store, key: '_cody_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CodyTest::Application.config.session_store :active_record_store
